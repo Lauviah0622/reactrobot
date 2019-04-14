d56acb84a7a568277d403e7dcca58fa7bd2ab3bf
Lauviah0622 <lauviah0622@gmail.com>
2019-04-14 23:14:34 +0800

    Updates

---
+++ precache-manifest.36e3c89357b90ab281f0ca49779f47dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74d3a2a084019d21c5ba",
+    "url": "/static/js/main.f4be514c.chunk.js"
+  },
+  {
+    "revision": "bf7d158ab286d562dbdd",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "74d3a2a084019d21c5ba",
+    "url": "/static/css/main.d0ace443.chunk.css"
+  },
+  {
+    "revision": "bf7d158ab286d562dbdd",
+    "url": "/static/css/2.50c8542d.chunk.css"
+  },
+  {
+    "revision": "bb6dcb285885607c7ce8a715de24743e",
+    "url": "/index.html"
+  }
+];
